3c8e60ce70831bf67e71c4e645d51fa4b698e7f0
Hanako Hashiguchi <hanako.e.hashiguchi@gmail.com>
2020-10-02 11:59:09 -0500

    added employees, locations, and customers

---
+++ src/components/employee/Employee.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./Employee.css"
+
+export const EmployeeCard = () => (
+    <section className="employee">
+        <h3 className="employee__name">Carl</h3>
+        <address className="employee__address">
+            <div>Nashville Kennels North</div>
+        </address>
+    </section>
+)
